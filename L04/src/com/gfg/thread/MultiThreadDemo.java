6c5b4282e0a1d79b133f9192373056196077f427
Anirban Chakraborty <anirban94chakraborty@gmail.com>
2024-07-31 23:24:20 +0530

    Add files for 'Multithreading' demo

---
+++ L04/src/com/gfg/thread/MultiThreadDemo.java
@@ -0,0 +1,25 @@
+package com.gfg.thread;
+
+import java.util.Scanner;
+
+public class MultiThreadDemo {
+
+    public static void main(String[] args) {
+
+        // By default, the 'main' thread will execute the following statements (instructions)
+        System.out.println("Hello");
+        System.out.println("Have a Good Day!");
+        System.out.println("Bye!");
+        System.out.println(Thread.currentThread().getName());
+
+        MyThread myThread = new MyThread(); // Object for creating child thread
+        myThread.setName("MyThread-01"); // Set name of child thread
+        myThread.start(); // Begin execution of child thread -> executes the run() method of MyThread class
+        try {
+            myThread.join();  // Instruct parent thread (main) to wait for child thread to terminate/complete execution
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Done by " + Thread.currentThread().getName());
+    }
+}
