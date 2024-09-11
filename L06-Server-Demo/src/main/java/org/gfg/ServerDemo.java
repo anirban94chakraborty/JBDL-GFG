388e1f8daa8fbc1459471bd0c0d46a38e6f1b911
Anirban Chakraborty <anirban94chakraborty@gmail.com>
2024-09-11 07:01:02 +0530

    Add code for demo on 'Simple Web Server Design'

---
+++ L06-Server-Demo/src/main/java/org/gfg/ServerDemo.java
@@ -0,0 +1,30 @@
+package org.gfg;
+
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ServerDemo {
+
+    public static void main(String[] args) {
+
+        System.out.println("Starting server");
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            System.out.println("Enter request data");
+            String data = sc.nextLine();
+            executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                   try {
+                       Thread.sleep(2000);
+                       System.out.println("Processing " + data + " in thread:" + Thread.currentThread().getName());
+                   } catch (InterruptedException e) {
+                       throw new RuntimeException(e);
+                   }
+                }
+            });
+        }
+    }
+}
---
+++ L06-Server-Demo/src/main/java/org/gfg/server/HelloHandler.java
@@ -0,0 +1,16 @@
+package org.gfg.server;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+
+public class HelloHandler implements HttpHandler {
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String msg = "Hello from " + Thread.currentThread().getName();
+        exchange.sendResponseHeaders(200, msg.length());
+        exchange.getResponseBody().write(msg.getBytes());
+    }
+}
