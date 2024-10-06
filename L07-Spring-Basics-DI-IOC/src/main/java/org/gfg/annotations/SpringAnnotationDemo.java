6333406625807eb3a90d8e697b69277c092aaf3f
Anirban Chakraborty <anirban94chakraborty@gmail.com>
2024-10-07 00:39:17 +0530

    Add code to show difference between Spring Beans and JVM Objects

--- L07-Spring-Basics-DI-IOC/src/main/java/org/gfg/annotations/SpringAnnotationDemo.java
+++ L07-Spring-Basics-DI-IOC/src/main/java/org/gfg/annotations/SpringAnnotationDemo.java
@@ -7,10 +7,16 @@ public class SpringAnnotationDemo {
 
     public static void main(String[] args) {
 
+        // Objects (Beans) created using dependency injection by the Spring IoC container
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext("org.gfg.annotations");
         NotificationService notificationService1 = applicationContext.getBean("notificationService", NotificationService.class);
         NotificationService notificationService2 = applicationContext.getBean("notificationService", NotificationService.class);
         System.out.println("notificationService1 == notificationService2 ? " + (notificationService1 == notificationService2)); // Result depends on the scope of the bean
         notificationService2.sendNotification("OTP:8909");
+
+        // Objects created directly in the JVM
+        NotificationService notificationServiceObj1 = new NotificationService();
+        NotificationService notificationServiceObj2 = new NotificationService();
+        System.out.println("notificationServiceObj1 == notificationServiceObj2 ? " + (notificationServiceObj1 == notificationService1));
     }
 }
