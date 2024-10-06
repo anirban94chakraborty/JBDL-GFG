5d772ad9f1872177d3a539a32860cdc19afe06f3
Anirban Chakraborty <anirban94chakraborty@gmail.com>
2024-10-07 00:42:54 +0530

    Modify code to allow creation of objects directly in JVM

--- L07-Spring-Basics-DI-IOC/src/main/java/org/gfg/annotations/NotificationService.java
+++ L07-Spring-Basics-DI-IOC/src/main/java/org/gfg/annotations/NotificationService.java
@@ -5,26 +5,26 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component // @Component annotation is used to mark a class as a bean
-@Scope("prototype") // @Scope annotation is used to define the scope of a bean
+// @Scope("prototype") // @Scope annotation is used to define the scope of a bean [Default Scope: singleton]
 public class NotificationService {
 
-    // Field-based Dependency Injection
+    // Field-based Dependency Injection [uses Java Reflection internally]
     @Autowired // @Autowired annotation is used to inject dependencies
     private SMSGatewayService smsGatewayService;
 
     // Constructor-based Dependency Injection (best approach; allows for Unit Testing)
     // No need to explicitly use @Autowired annotation for constructor-based DI
-    public NotificationService(SMSGatewayService smsGatewayService) {
-        System.out.println("Executing constructor");
-        this.smsGatewayService = smsGatewayService;
-    }
-
-    // Setter-based Dependency Injection (allows for Unit Testing)
-//    @Autowired
-//    public void setSmsGatewayService(SMSGatewayService smsGatewayService) {
-//        System.out.println("Executing setter");
+//    public NotificationService(SMSGatewayService smsGatewayService) {
+//        System.out.println("Executing constructor");
 //        this.smsGatewayService = smsGatewayService;
 //    }
+
+    // Setter-based Dependency Injection (allows for Unit Testing)
+    @Autowired
+    public void setSmsGatewayService(SMSGatewayService smsGatewayService) {
+        System.out.println("Executing setter");
+        this.smsGatewayService = smsGatewayService;
+    }
 
     public void sendNotification(String msg) {
         smsGatewayService.sendSMS(msg);
