9d8236b514e06df135b8a494cf1086f26730a2c5
Anirban Chakraborty <anirban94chakraborty@gmail.com>
2024-10-14 23:59:08 +0530

    Add code for demo on sending data in GET and POST requests

--- L07-spring-boot-demo/src/main/java/com/example/L07_spring_boot_demo/AppController.java
+++ L07-spring-boot-demo/src/main/java/com/example/L07_spring_boot_demo/AppController.java
@@ -1,10 +1,7 @@
 package com.example.L07_spring_boot_demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/app")
@@ -21,14 +18,22 @@ public class AppController {
         return "Hello from: L07 app";
     }
 
+    /* Sending Data in Request using Request Parameters / Query Parameters */
     @GetMapping("/hello")
-    public String getHello() {
-        return "Hello from: " + Thread.currentThread().getName();
+    public String getHello(@RequestParam(required = false, name = "nm", defaultValue = "Sample") String name) {
+        return "Hello! " + name + " from: " + Thread.currentThread().getName();
     }
 
+    /* Sending Data in Request using Path Parameters */
     @GetMapping("/product/{id}")
     public Product getProductById(@PathVariable Long id) {
         return productService.getProductById(id);
+    }
+
+    /* Sending Data in Request using Request Body */
+    @PostMapping("createProduct")
+    public Product createProduct(@RequestBody Product product) {
+        return productService.createProduct(product);
     }
 
     @GetMapping("/testUrl")
--- L07-spring-boot-demo/src/main/java/com/example/L07_spring_boot_demo/ProductService.java
+++ L07-spring-boot-demo/src/main/java/com/example/L07_spring_boot_demo/ProductService.java
@@ -5,21 +5,29 @@ import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class ProductService {
 
     private Map<Long, Product> productMap;
+    private AtomicLong nextId = new AtomicLong(1l);
 
     @PostConstruct
     public void initMethod() {
         productMap = new HashMap<>();
-        productMap.put(1L, new Product(1L, "Laptop", 50000.0));
-        productMap.put(2L, new Product(2L, "Mouse", 500.00));
-        productMap.put(3L, new Product(3L, "Keyboard", 1000.00));
+        productMap.put(nextId.getAndIncrement(), new Product(1L, "Laptop", 50000.0));
+        productMap.put(nextId.getAndIncrement(), new Product(2L, "Mouse", 500.00));
+        productMap.put(nextId.getAndIncrement(), new Product(3L, "Keyboard", 1000.00));
     }
 
     public Product getProductById(Long id) {
         return productMap.get(id);
+    }
+
+    public Product createProduct(Product product) {
+        product.setId(nextId.getAndIncrement());
+        productMap.put(product.getId(), product);
+        return product;
     }
 }
