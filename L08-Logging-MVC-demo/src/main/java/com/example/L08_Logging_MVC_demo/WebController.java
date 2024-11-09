47ad3172cef01bd7de1c9f68dc48f94b266ccd98
Anirban Chakraborty <anirban94chakraborty@gmail.com>
2024-11-09 22:08:08 +0530

    Add code for demo on '@Controller' & '@RestController' in Spring MVC

--- L08-Logging-MVC-demo/src/main/java/com/example/L08_Logging_MVC_demo/WebController.java
+++ L08-Logging-MVC-demo/src/main/java/com/example/L08_Logging_MVC_demo/WebController.java
@@ -2,10 +2,8 @@ package com.example.L08_Logging_MVC_demo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.text.SimpleDateFormat;
@@ -13,23 +11,15 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-@RestController
-@RequestMapping("/app")
-public class AppController {
+@Controller
+public class WebController {
 
-    private static Logger LOGGER = LoggerFactory.getLogger(AppController.class);
+    private static Logger LOGGER = LoggerFactory.getLogger(WebController.class);
 
-    @GetMapping
-    public String hello(@RequestParam String name) {
-        LOGGER.info("Processing hello API for data: {}", name);
-        LOGGER.error("Some error");
-        LOGGER.warn("Some warning");
-        LOGGER.debug("Some debug");
-        return "Hello " + name + " from: " + Thread.currentThread().getName();
-    }
+    @GetMapping("/dynamic-list")
+    public ModelAndView getList() {
 
-    @GetMapping("productList")
-    public List<Product> getList() {
+        LOGGER.info("Processing dynamic-list API");
         List<Product> productList = new ArrayList<>();
         productList.add(new Product(1l, "Mouse", 5000.00));
         productList.add(new Product(2l, "Keyboard", 6000.00));
@@ -38,6 +28,19 @@ public class AppController {
         modelAndView.getModelMap().put("products", productList);
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
         modelAndView.getModelMap().put("serverTime", dateFormat.format(new Date()));
+        return modelAndView;
+    }
+
+    // Call to this endpoint will result in a 500 error
+    // because @Controller treats the return value as a view name
+    // and not an HTTP response (serialized to JSON)
+    @GetMapping("/products")
+    public List<Product> getProductList() {
+
+        List<Product> productList = new ArrayList<>();
+        productList.add(new Product(1l, "Mouse", 5000.00));
+        productList.add(new Product(2l, "Keyboard", 6000.00));
+        productList.add(new Product(3l, "Monitor", 7000.00));
         return productList;
     }
 }
